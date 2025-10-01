216aee38bd63fc5253b397db0d3db985568cc1bb
0xD4rky <ishaan.kumar281@gmail.com>
2025-10-01 12:01:12 +0530

    written code for base-class based conversion of datasets

---
+++ data/base.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, asdict
+from typing import Any, Dict, List
+import json
+
+@dataclass
+class SFT_Config:
+    messages: List[Dict[str, str]]
+
+    def to_dict(self) -> Dict[str, Any]: # returns a dict of the dataclass
+        return asdict(self)
+
+
+class BaseSFTDataset(ABC):
+
+    def __init__(self, name: str):
+        self.name = name
+
+    @abstractmethod
+    def load_dataset(self) -> List[Dict[str, Any]]:
+        pass
+    
+    @abstractmethod
+    def convert(self, example: Dict[str, Any]) -> SFT_Config:
+        pass
+    
+    def parse_dataset(self) -> List[SFT_Config]:
+        """
+        function to parse and convert the dataset into a unified format for SFT
+        """
+
+        dataset = self.load_dataset()
+        return [self.convert(example) for example in dataset]
+    
+    def save_dataset(self, output_path: str):
+
+        dataset = self.parse_dataset()
+        with open(output_path, "w") as f:
+            for example in dataset:
+                f.write(json.dumps(example.to_dict()) + "\n")
+                
+        print(f"Dataset saved to {output_path}, length: {len(dataset)}")
