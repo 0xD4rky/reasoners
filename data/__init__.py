4446324723d7ddcf63c30a1c174e9f80e74250bc
0xD4rky <ishaan.kumar281@gmail.com>
2025-10-13 08:33:57 +0530

    support fixes

--- data/__init__.py
+++ data/__init__.py
@@ -3,5 +3,6 @@ from .factory import DataFactory
 from .datasets.openthoughts2 import OpenThoughts2Dataset
 from .datasets.gsm8k import GSM8KDataset
 from .data_mixer import DataMixer
+from .datasets.stratos import StratosDataset
 
-__all__ = ["BaseSFTDataset", "SFTConfig", "DataFactory", "OpenThoughts2Dataset", "DataMixer", "GSM8KDataset"]
+__all__ = ["BaseSFTDataset", "SFTConfig", "DataFactory", "DataMixer", "OpenThoughts2Dataset", "GSM8KDataset", "StratosDataset"]
--- data/datasets/stratos/stratos.py
+++ data/datasets/stratos/stratos.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 class StratosDataset(BaseSFTDataset):
 
     def load_data(self) -> Dataset:
-        return load_dataset("bespokelabs/Bespoke-Stratos-17k", "main", split="train")
+        return load_dataset("bespokelabs/Bespoke-Stratos-17k", split="train")
     
     def convert(self, example: Dict[str, Any]) -> SFTConfig:
 
@@ -28,7 +28,7 @@ class StratosDataset(BaseSFTDataset):
 
         dataset = self.load_data()
         messages = []
-        for i in tqdm(range(len(dataset)), desc="Parsing GSM8K dataset"):
-            messages.append(self.convert(dataset[i]))
+        for i in tqdm(range(len(dataset)), desc="Parsing stratos dataset"):
+            messages.append(self.convert(dataset[i]["conversations"]))
         
         return messages
